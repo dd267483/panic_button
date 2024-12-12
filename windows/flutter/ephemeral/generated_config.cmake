f7003af995c39c53e55298562c05a6f08efd7775
dd267483 <dd260910@gmail.com>
2024-12-12 11:43:02 +0200

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "P:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "P:\\FlutterProject\\panic_button" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=P:\\flutter"
+  "PROJECT_DIR=P:\\FlutterProject\\panic_button"
+  "FLUTTER_ROOT=P:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=P:\\FlutterProject\\panic_button\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=P:\\FlutterProject\\panic_button"
+  "FLUTTER_TARGET=P:\\FlutterProject\\panic_button\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=P:\\FlutterProject\\panic_button\\.dart_tool\\package_config.json"
+)
